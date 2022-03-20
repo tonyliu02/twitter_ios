7295f1a11521131640099d4866300a6e7d7d82da
Tony Liu <tliu5175@gmail.com>
2022-03-19 22:52:19 -0700

    added gif walkthrough

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -16,7 +16,6 @@ class TweetViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         tweetTextView.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
